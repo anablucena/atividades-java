ad70e0aba6b839f75091289489405be523bcac83
Aninha <apholium@gmail.com>
2026-04-18 14:40:10 -0400

    20260418-ana-feat-imprimir-ola-mundo

--- exercicios/Exercicio001.java
+++ exercicios/Exercicio001.java
@@ -2,6 +2,6 @@ package exercicios;
 
 public class Exercicio001 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Olá, mundo!!");
     }
 }
